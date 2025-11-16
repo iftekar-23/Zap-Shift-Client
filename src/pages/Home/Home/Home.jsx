3835a32503670a3f4a562c2128ca4a4897cb42e7
iftekar-23 <aiftekar798@gmail.com>
2025-11-16 19:08:37 +0600

    added brands and reviews

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import HowItWorks from '../HowItWorks/HowItWorks';
 import OurServices from '../OurServices/OurServices';
+import Brands from '../Brands/Brands';
+import Reviews from '../../Reviews/Reviews';
+
+
+const reviewsPromise = fetch('/reviews.json').then(res => res.json());
 
 const Home = () => {
     return (
@@ -10,8 +15,14 @@ const Home = () => {
            <section>
             <HowItWorks></HowItWorks>
            </section>
-           <section>
+           <section className='my-5'>
             <OurServices></OurServices>
+           </section>
+           <section className='mt-10'>
+                <Brands></Brands>
+           </section>
+           <section>
+            <Reviews reviewsPromise={reviewsPromise}></Reviews>
            </section>
         </div>
     );
